50ecc6571bb87845baa8be2c5fb575cf349c5e54
tolkoxa <tolkoxa@ya.ru>
2021-03-25 22:51:27 +0300

    Добавляю js

--- js/eatfit.js
+++ js/eatfit.js
@@ -1,9 +1,23 @@
 class eatfit {
     constructor() {
+        this.slider = document.getElementById('slider');
+        this.colorblock = document.getElementById('colorblock');
         this._init();
     }
 
-    _init() {}
+    _init() {
+        let random = this.randomizer(3);
+        this.randomSlider(random);
+    }
+
+    randomSlider(number) {
+        this.slider.classList.add(`slider-${number}`);
+        console.log(number);
+    }
+
+    randomizer(i) {
+        return Math.floor(Math.random() * i);
+    }
 
 }
 
